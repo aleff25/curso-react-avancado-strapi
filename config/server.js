c2ef0a074dce905188682122ac2044a7053ffcc4
Aleff <aleff2594@gmail.com>
2020-07-30 09:47:33 -0300

    First Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8fa58a98b23e09a5c648270981c90056'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ec87c012-f7bc-41ee-a637-a3b821ea0eb8'
+};
